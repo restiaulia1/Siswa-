32231ff8b17d97867f07806b6125ebbcdf24511f
Resti <restiaulia480@gmail.com>
2024-08-12 10:55:14 +0700

    mengimpor library firestore

--- main.js
+++ main.js
@@ -1,5 +1,16 @@
 import { initializeApp } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js'
-import { getFirestore } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  addDoc,
+  deletDoc,
+  updateDoc,
+  query,
+  orderBy
+  } from 'https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js'
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
   apiKey: "AIzaSyBoQovSZfN-IWxwE6SNigeVFl7EyoGo6I8",
